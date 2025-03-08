12e633958257892c4767b8f3476f57a887706ed2
Andres H <adavidhern15@outlook.com>
2025-03-08 18:21:35 -0500

    finalizing introduction

--- app/query/route.ts
+++ app/query/route.ts
@@ -1,26 +1,37 @@
-// import postgres from 'postgres';
+import postgres from 'postgres';
+import bcrypt from 'bcryptjs';
+const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 
-// const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
+async function listInvoices() {
+	// const data = await sql`
+  //   SELECT invoices.amount, customers.name
+  //   FROM invoices
+  //   JOIN customers ON invoices.customer_id = customers.id
+  //   WHERE invoices.amount = 666;
+  // `;
+  const hashedPassword = await bcrypt.hash('123456', 10);
+  // const data = await sql`
+  //   SELECT * FROM invoices
+  // `;
+  const data = await sql`
+    INSERT INTO users (id, name, email, password)
+    VALUES ('410544b2-4001-4271-9855-fec4b6a6442a', 'User', 'user@nextmail.com', ${hashedPassword});
+`;
+  // const data = await sql`
+  //   DROP DB supabase-rose-island 
+  // `;
 
-// async function listInvoices() {
-// 	const data = await sql`
-//     SELECT invoices.amount, customers.name
-//     FROM invoices
-//     JOIN customers ON invoices.customer_id = customers.id
-//     WHERE invoices.amount = 666;
-//   `;
-
-// 	return data;
-// }
+	return data;
+}
 
 export async function GET() {
-  return Response.json({
-    message:
-      'Uncomment this file and remove this line. You can delete this file when you are finished.',
-  });
-  // try {
-  // 	return Response.json(await listInvoices());
-  // } catch (error) {
-  // 	return Response.json({ error }, { status: 500 });
-  // }
+  // return Response.json({
+  //   message:
+  //     'Uncomment this file and remove this line. You can delete this file when you are finished.',
+  // });
+  try {
+  	return Response.json(await listInvoices());
+  } catch (error) {
+  	return Response.json({ error }, { status: 500 });
+  }
 }
